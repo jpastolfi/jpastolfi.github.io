36761cac69602e35adb03075d5790f85fa1a5b91
Joao Astolfi <jpastolfi@gmail.com>
2024-08-09 10:01:36 +0100

    Folders setup, Home component and Card component

---
+++ src/app/components/card-link/card-link.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-link.component.html',
+  styleUrl: './card-link.component.scss'
+})
+export class CardLinkComponent {
+
+}
